ca8c09de0edd50c263fbd42a1a76f573277973a8
xiaokang <352389286@qq.com>
2016-01-21 12:52:11 +0800

    Initial Commit

---
+++ SwuAssistant/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SwuAssistant
+//
+//  Created by ShockHsu on 16/1/21.
+//  Copyright © 2016年 OpenSource Association of SWU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SwuAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwuAssistant
+//
+//  Created by ShockHsu on 16/1/21.
+//  Copyright © 2016年 OpenSource Association of SWU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
