2697242037de6d042da4af883943e249bbcbb740
xuxiaokang <352389286@qq.com>
2016-04-11 23:04:06 +0800

    UPDATE:  UPDATE POD SETTINGS.

--- SwuAssistantTests/SwuAssistantTests.m
+++ SwuAssistantTests/SwuAssistantTests.m
@@ -27,19 +27,6 @@
     [super tearDown];
 }
 
-//- (void)testExample {
-//    XCTAssert(1 == 1);
-//    // This is an example of a functional test case.
-//    // Use XCTAssert and related functions to verify your tests produce the correct results.
-//}
-//
-//- (void)testPerformanceExample {
-//    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
-//}
-
 - (void)testLogin {
     Router *r = [Router sharedInstance];
     XCTestExpectation *expectation = [self expectationWithDescription:@"test"];
